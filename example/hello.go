b255e06523462e52efe5d57e531716b1107eda57
lucascerci <lucas.fernandes001p@gmail.com>
2020-10-04 14:56:51 -0300

    variables

--- example/hello.go
+++ example/hello.go
@@ -3,5 +3,20 @@ package main
 import ("fmt")
 
 func main() {
-	fmt.Println("Hello World");
+	//first variable
+	var x int
+	//if dont provide any value it will return 0 for every type of var
+	fmt.Println("var with no value = ", x)
+
+	var h int = 5
+	var y int = 7
+	var sum int = h + y
+
+	fmt.Println("sum", sum)
+
+	q := 5
+	p := 7
+	soma := q + p
+
+	fmt.Println("sum without defining var", soma)
 }
